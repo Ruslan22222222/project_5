e5dccfa544c769e3145a9b5f6aaed93bac5fb2c1
Ruslan22222222 <157678831+Ruslan22222222@users.noreply.github.com>
2024-01-31 21:26:28 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -8,48 +8,42 @@ def add(n1, n2):
 def substract(n1, n2):
     return n1 - n2
 
-def myltiply(n1, n2):
+def multiply(n1, n2):
     return n1 * n2
 
 def divide(n1, n2):
     return n1 / n2
 
 option = {
-"+": add,
-"-": substract,
-"*": myltiply,
-"/": divide
+    "+": add,
+    "-": substract, 
+    "*": multiply,
+    "/": divide
 }
 
 def calculator():
     print(logo)
-    
-    while True:
-        num1 = input("What's your first number:? ")
-        if num1.isdigit():
-            num = float(num1)
-            break
-        else:
-            print("Invalid input. Please enter a valid number.")
-    
+    num1 = float(input("What's your first numbder?: "))
     for symbol in option:
-        print(symbol)
+        print(symbol) # Вывод доступных операторов
 
     should_countinue = True
 
     while should_countinue:
         symbol = input("Pick an option: ")
-        num2 = float(input("WHat's your second number?: "))
-        calculation = option[symbol]
-        answer = calculation(num1, num2)
-    print(f"{num1} {symbol} {num2} = {answer}")
+        num2 = float(input("What's a second number"))
+        calculation = option[symbol] # Выбор соответствующей функции из словаря по введенному оператору
+        answer = calculation(num1, num2) # Выполнение выбранной операции
 
-    if input("Type 'yes' or 'no'. ") == "yes":
-        num = answer
-        
-    else:
-        os.system('clear')
-        should_countinue = False
-        calculator()
+        print(f"{num1} {symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to countinue calculating with {answer}, or type 'no' to start a new calculation: ") == "yes":
+            num1 = answer
+
+        else:
+            os.system('clear')
+            should_countinue = False
+            calculator()
+
 
 calculator()
